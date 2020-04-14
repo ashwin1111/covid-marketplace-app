89a3a63578fd1de574f123f38f668630908536d3
ashwin1111 <winash1998@gmail.com>
2020-04-14 17:54:38 +0530

    firebase hosting

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to url-shortener-app!');
+    expect(page.getTitleText()).toEqual('Welcome to covid19-marketplace!');
   });
 
   afterEach(async () => {
